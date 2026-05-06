b8804d0ed20d73a0a6abbd60512f318bd697fc8a
gtats <gtatsoglou@gmail.com>
2026-05-06 15:36:14 +0300

    restructure project

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, Area, AreaChart } from 'recharts';
 import { Thermometer, Activity, TrendingUp, Clock } from 'lucide-react';
